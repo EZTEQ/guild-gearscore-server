19f8bf1b754df4cd3c5213455aa386ac881eafc1
Mark Hähnel <hello@markhaehnel.de>
2016-09-23 15:21:06 +0200

    More guild view stuff

--- log/log.js
+++ log/log.js
@@ -1,5 +1,5 @@
 /* huge module for request logging */
 module.exports = (req, res, next) => {
-	console.log(new Date().toLocaleString(), req.method, req.path);
+	console.log(new Date().toUTCString(), req.method, req.path);
 	next();
 }
