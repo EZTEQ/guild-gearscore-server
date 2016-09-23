f4dfb22049db60e204ff54cb65fefdd0a7d08025
Mark Hähnel <hello@markhaehnel.de>
2016-09-23 10:40:07 +0200

    Added guild view

--- routes/web.js
+++ routes/web.js
@@ -5,9 +5,15 @@ module.exports = (() => {
 	let router = express.Router();
 
 	router.get('/', (req, res) => {
-		res.render('home', {
+		res.render('home');
+	});
+
+	router.get('/:realm/:guild', (req, res) => {
+		res.render('guild-overview', {
 			helpers: {
-				title: 'Home'
+				title: req.params.guild + ' - ' + req.params.realm,
+				realm: req.params.realm,
+				guild: req.params.guild
 			}
 		});
 	});
