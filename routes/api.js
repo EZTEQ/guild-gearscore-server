06646db2045905b1705bc99f53171455ce3da782
Mark Hähnel <hello@markhaehnel.de>
2016-11-10 14:51:35 +0100

    Updated node modules

--- routes/api.js
+++ routes/api.js
@@ -7,12 +7,12 @@ module.exports = (() => {
 
     router.get('/:type(guild|character)/:realm/:param/:field', cache('60 minutes'), (req, res) => {
         wow.call(req.params,
-            function successCallback(response) {
+            (response) => {
                 res.status(response.status);
                 res.json(response.data);
             },
-            function errorCallback(error) {
-                //TODO Add logging
+            (error) => {
+                // TODO Add logging
                 res.status(error.response.status);
                 res.json(error.response.data);
             });
